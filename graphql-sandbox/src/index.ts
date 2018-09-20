59b36567911527d0b8c44c6e3e3c06da52a10fa7
chrisamurao <freshpotatoes@gmail.com>
2018-09-20 02:19:40 -0700

    separated resolvers, created typeorm entity for u/p

--- graphql-sandbox/src/index.ts
+++ graphql-sandbox/src/index.ts
@@ -1,4 +1,3 @@
-import "reflect-metadata";
 // import {createConnection} from "typeorm";
 // import {User} from "./entity/User";
 
@@ -11,30 +10,20 @@ import "reflect-metadata";
 //     user.age = 25;
 //     await connection.manager.save(user);
 //     console.log("Saved a new user with id: " + user.id);
-    
+
 //     console.log("Loading users from the database...");
 //     const users = await connection.manager.find(User);
 //     console.log("Loaded users: ", users);
-     
+
 //     console.log("Here you can setup and run express/koa/any other framework.");
-    
+
 // }).catch(error => console.log(error));
 
-import { GraphQLServer } from 'graphql-yoga'
-// ... or using `require()`
-// const { GraphQLServer } = require('graphql-yoga')
+import "reflect-metadata";
+import { GraphQLServer } from "graphql-yoga";
+import { importSchema } from "graphql-import";
+import { resolvers } from "./resolvers";
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
-  },
-}
-
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000')) 
+const typeDefs = importSchema("schema.graphql");
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
